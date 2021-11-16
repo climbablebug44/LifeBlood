a4c6374a654349e55425bf31b24a8bc5a34c6da5
Your Name <climbablebug_44@protonmail.com>
2021-11-16 12:33:20 +0530

    added fetch to profile page

--- lifeblood/src/Components/ProfilePage/about.js
+++ lifeblood/src/Components/ProfilePage/about.js
@@ -1,53 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './about.module.css';
 import image from '../../assets/victer.jpeg';
 
 const About = () => {
 
-    const user = {
-        name: 'Victer Paul',
-        bloodGrp: 'O-',
-        email: 'victerpaul@iiitkottayam.ac.in',
-        phone: 9898989898,
-        pendingDonateRequest: 2,
-        noOfTimesDonated: 3,
-        requestPostonFeed: 'camnjhda12',
-        imgSrc: image
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ userID: localStorage.getItem("userId") })
     }
-    return (
-        <div className={styles.container}>
-            <div className={styles.contain}>
-                <div className={styles['container-1']}>
-                    <div className={styles.image}>
-                        <img src={image} />
+
+    const [user, setUser] = useState({});
+
+    const changeUser = (newUser) => {
+        setUser(newUser);
+    }
+
+    var req = "http://localhost:4000/api/profile";
+
+    useEffect(() => {
+        fetch(req, requestOptions).then(
+            response => {
+                if (response.status !== 200) {
+                    throw new Error("invalid response from server");
+                }
+                return response.json()
+            }
+        ).then(data_ => {
+            changeUser(data_)
+        }).catch(error => {
+            console.log("[error-log]: ", error);
+        });
+    }, [])
+
+    console.log("user = ", user)
+
+    if (user != null)
+        return (
+            <div className={styles.container}>
+                <div className={styles.contain}>
+                    <div className={styles['container-1']}>
+                        <div className={styles.image}>
+                            <img alt="<profile photu>" src={image} />
+                        </div>
+                        <div className={styles.name}>
+                            {user.name}
+                        </div>
+                        <div className={styles.bloodGrp}>
+                            Blood Group (<span>{user.blood_group}</span>)
+                        </div>
                     </div>
-                    <div className={styles.name}>
-                        {user.name}
-                    </div>
-                    <div className={styles.bloodGrp}>
-                        Blood Group (<span>{user.bloodGrp}</span>)
-                    </div>
-                </div>
-                <div className={styles['container-2']}>
-                    <div className={styles.email}>
-                        {user.email}
-                    </div>
-                    <div className={styles.phone}>
-                        {user.phone}
-                    </div>
-                    <div className={styles.number}>
-                        {user.pendingDonateRequest}
-                    </div>
-                    <div className={styles.number}>
-                        {user.noOfTimesDonated}
-                    </div>
-                    <div className={styles.id}>
-                        <a href='#'>Go to your request</a>
+                    <div className={styles['container-2']}>
+                        <div className={styles.email}>
+                            {user.email}
+                        </div>
+                        <div className={styles.phone}>
+                            {user.phone_number}
+                        </div>
+                        <div className={styles.number}>
+                            {user.pendingDonateRequest}
+                        </div>
+                        <div className={styles.number}>
+                            {user.noOfTimesDonated}
+                        </div>
+                        <div className={styles.id}>
+                            <a href='#'>Go to your request</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    else
+        return(<React.Fragment/>)
 }
 
 export default About;
