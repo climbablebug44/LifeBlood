f3f542a7b4a8020cfce05ad54cd17a1b0eb5c630
ashish <ash569sharma@gmail.com>
2021-11-19 13:06:30 +0530

    changes

--- backend/lifeblood/src/Components/modalForShare/ModalForShare.js
+++ backend/lifeblood/src/Components/modalForShare/ModalForShare.js
@@ -5,10 +5,7 @@ import ShareNoModal from '../ShareNoModal/ShareNoModal';
 const ModalForShare = (props)=>{
     return (
         <React.Fragment>
-<<<<<<< HEAD
-           
-=======
->>>>>>> 1ef5ff9a3bb16a3e6542fffa97febb8d1aa53550
+
             <ShareNoModal handleClose = {props.handleClose}/>
         </React.Fragment>
     )
